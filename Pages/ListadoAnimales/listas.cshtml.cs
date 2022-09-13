424b0bc742936f3b6ae1c6238348bab239ed1896
Guillermo Romero <92944164+memoromero1972@users.noreply.github.com>
2022-09-12 19:51:46 -0500

    Sprint 2 Version 1.3

--- Pages/ListadoAnimales/listas.cshtml.cs
+++ Pages/ListadoAnimales/listas.cshtml.cs
@@ -7,11 +7,16 @@ namespace Holaweb.App.Frontend.Pages
     public class listasModel : PageModel
     {
         private string[] vectorLista = { "Caballo Loco", "Plata", "Palomo", "Azabache", "Trueno", "Petronilo", "Ivancho", "Alvarito","Roycillo", "Rocinante", "MafeCabal" };
+        private string[] vectorPropietarios={"Andres","Pedro","Pablo","Moises","Andres","Pedro","Pablo","Moises","Andres","Pedro","Pablo"};
         public List<string> mylista { get; set; }=default!;
+        public List<string> mylista1 {get; set;}=default!;
         public void OnGet()
         {
             mylista = new List<string>();
+            mylista1 = new List<string>();
             mylista.AddRange(vectorLista);
+            mylista1.AddRange(vectorPropietarios);
+    
         }
     }
 }
